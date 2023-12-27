9a62440190b897b1c3ea979d2d7090977546a4ca
Christian Becker <christian@dabecka.de>
2023-12-27 02:17:19 +0100

    migrate off setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,6 @@
-import os
-
 from setuptools import setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(
-    name='Flask-Bower',
-    version='1.3.0',
-    url='http://github.com/lobeck/flask-bower',
-    license='GPLv2',
-    author='Christian Becker',
-    author_email='christian@dabecka.de',
-    description='An extension to manage and serve your javascript assets with bower',
-    long_description=read('README.rst'),
     packages=['flask_bower'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask>=0.9',
-    ],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
 )
